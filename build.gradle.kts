016e6f504bef445db9947edcf7fbe82184a6e827
ProjectTL12345 <owner@projecttl.net>
2021-06-24 11:09:54 +0900

    script: fix publish error

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,7 +52,7 @@ tasks {
 
     shadowJar {
         archiveBaseName.set(project.name)
-        archiveVersion.set("")
+        archiveVersion.set(project.version.toString())
         archiveClassifier.set("")
     }
 
