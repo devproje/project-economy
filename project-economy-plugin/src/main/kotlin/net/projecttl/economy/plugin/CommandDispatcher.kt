8da0071d0b480db49845909b66fb5427120cd67e
Project_TL <me@projecttl.net>
2022-06-07 20:55:02 +0900

    feat: middle save #2

--- project-economy-plugin/src/main/kotlin/net/projecttl/economy/plugin/CommandDispatcher.kt
+++ project-economy-plugin/src/main/kotlin/net/projecttl/economy/plugin/CommandDispatcher.kt
@@ -27,10 +27,12 @@ object CommandDispatcher : CommandExecutor {
             val player: Player = try {
                 Bukkit.getPlayer(args[1])!!
             } catch (exception: Exception) {
-                sender.sendMessage(Component.text("온라인 플레이어 이름을 입력해 주세요!", NamedTextColor.RED))
+                sender.sendMessage(
+                    Component.text("온라인 플레이어 이름을 입력해 주세요!", NamedTextColor.RED)
+                )
                 return false
             }
-            val money = Economy(player)
+            val target = Economy(player)
             val amount: Int = try {
                 Integer.parseInt(args[2])
             } catch (exception: Exception) {
@@ -44,7 +46,7 @@ object CommandDispatcher : CommandExecutor {
 
             return when (args[0]) {
                 "add" -> {
-                    res = money.addMoney(amount)
+                    res = target.addMoney(amount)
                     if (!res) {
                         sender.sendMessage(Component.text("0 또는 음수 값을 입력하실 수 없습니다.", NamedTextColor.RED))
                     }
@@ -53,20 +55,54 @@ object CommandDispatcher : CommandExecutor {
                 }
 
                 "drop" -> {
-                    res = money.dropMoney(amount)
+                    res = target.dropMoney(amount)
                     if (!res) {
-                        sender.sendMessage(Component.text("0 또는 음수 값 보유 계좌보다 더 큰 금액을 뺄 수 없습니다.", NamedTextColor.RED))
+                        sender.sendMessage(
+                            Component.text("0 또는 음수 값 보유 계좌보다 더 큰 금액을 뺄 수 없습니다.", NamedTextColor.RED)
+                        )
                     }
 
                     true
                 }
 
                 "set" -> {
-                    money.money = amount
+                    target.money = amount
                     player.sendMessage(Component
-                        .text("${money.player}", NamedTextColor.YELLOW)
+                        .text("${target.player}", NamedTextColor.YELLOW)
                         .append(Component.text("님의 계좌를 ", NamedTextColor.GREEN))
-                        .append(Component.text("$money${moneyUnit}으로 설정 하였습니다.", NamedTextColor.GREEN)))
+                        .append(Component.text("${target.money}${moneyUnit}으로 설정 하였습니다.", NamedTextColor.GREEN)))
+
+                    true
+                }
+
+                "send" -> {
+                    if (sender !is Player) {
+                        sender.sendMessage(
+                            Component.text("이 명령어는 오직 플레이어만 사용할 수 있습니다.", NamedTextColor.RED)
+                        )
+
+                        return false
+                    }
+
+                    val me = Economy(sender)
+
+                    val meRes = me.dropMoney(amount)
+                    if (!meRes) {
+                        me.player.sendMessage(
+                            Component.text("잔고가 없거나 보낼 금액이 0 또는 음수 입니다.", NamedTextColor.RED)
+                        )
+                        return true
+                    }
+
+                    target.addMoney(amount)
+
+                    me.player.sendMessage(Component.text(target.player.name, NamedTextColor.YELLOW).append(
+                        Component.text("님에게 $amount${moneyUnit}을(를) 보냈습니다.", NamedTextColor.GREEN))
+                    )
+
+                    target.player.sendMessage(Component.text(me.player.name, NamedTextColor.YELLOW).append(
+                        Component.text("님에게 $amount${moneyUnit}을(를) 받았습니다.", NamedTextColor.GREEN))
+                    )
 
                     true
                 }
