e2de5b8e93bda390131e5a1ece7982c2920fda68
projecttl <me@projecttl.net>
2022-06-10 12:45:16 +0900

    feat: middle save #4

--- project-economy-plugin/src/main/kotlin/net/projecttl/economy/plugin/CommandDispatcher.kt
+++ project-economy-plugin/src/main/kotlin/net/projecttl/economy/plugin/CommandDispatcher.kt
@@ -1,11 +1,24 @@
 package net.projecttl.economy.plugin
 
 import io.github.monun.kommand.PluginKommand
+import io.github.monun.kommand.getValue
 import net.projecttl.economy.core.Economy
+import org.bukkit.Bukkit
 
 object CommandDispatcher {
     fun register(kommand: PluginKommand) {
         kommand.register("money") {
+            then("send") {
+                requires { isPlayer }
+                executes { context ->
+                    val userName: String by context
+                    val target = try {
+                        Bukkit.getPlayer(userName)
+                    } catch (exception: Exception) {
+                        return@executes
+                    }
+                }
+            }
             requires { isPlayer }
             executes {
                 val economy = Economy(player)
